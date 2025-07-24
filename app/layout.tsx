3dda35d573552675f05859fa64547c064e81975a
Angela <226angelli@gmail.com>
2025-07-23 22:49:01 -0700

    testing pt2

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
         <link 
           rel="stylesheet" 
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
